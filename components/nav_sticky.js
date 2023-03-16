894bbc9d3bb7100cbf55581e058421a12d928f85
Arian <arianmrv12@gmail.com>
2023-03-16 18:28:25 -0500

    navbar sticky added

---
+++ components/nav_sticky.js
@@ -0,0 +1,11 @@
+const navbar_top = document.querySelector('.navbar').getBoundingClientRect().top;
+console.log(navbar_top);
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset >= navbar_top) {
+        const navbar = document.querySelector('.navbar');
+        navbar.style.cssText = 'position: fixed; top: 0';
+    }else {
+        const navbar = document.querySelector('.navbar');
+        navbar.style.cssText = 'position: inherit;';
+    }
+});
